c837356f66b14258e12139e469bcff53f4039e10
Tanya Pavlishina <tanya.pavlishina@gmail.com>
2017-06-14 20:59:21 +0300

    hoty booes

---
+++ src/main/java/com/example/demo/Bi3PavlishinaNew3Application.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bi3PavlishinaNew3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bi3PavlishinaNew3Application.class, args);
+	}
+}
